067c9cb1d84d155acbb4b12ef347990129baa701
Arpita <arpitasawant2002@gmail.com>
2023-08-12 11:10:56 +0530

    Updated

--- src/Items.js
+++ src/Items.js
@@ -4,42 +4,84 @@ const items = [
         name: "BBQ",
         desc: "BBQ sauce with seasoned grilled chicken",
         img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.chicken-lovers.b954bbe4c3fed2d6e1bdb2953902be9e.1.jpg?width=300",
-        price: 12.5
+        price: 199
     },
     {
         id: 2,
         name: "Supreme",
         desc: "Italian sausage, ham, black olive",
         img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.super-supreme.21f3260bdd33813f550cd523fbd8d073.1.jpg?width=300",
-        price: 14.5
+        price: 399
     },
     {
         id: 3,
         name: "Canadian",
         desc: "Bacon, ham sausage",
         img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.canadian.67c22dc316f702bf4d992c667d22fbf0.1.jpg?width=300",
-        price: 12.5
+        price: 299
     },
     {
         id: 4,
         name: "Veggie",
         desc: "Sliced mushrooms, green pepper, red onion, tomato and pizza mozzarella",
         img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.veggie-lovers.48ba54d49ff2257671521b477967633a.1.jpg?width=300",
-        price: 11.5
+        price: 99
     },
     {
         id: 5,
         name: "Jalapeno",
         desc: "Italian sausage, pepperoni, sliced mushroom",
         img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.italian-classic.ad7765e7d0bfed6e00b09053416cba63.1.jpg?width=300",
-        price: 14.5
+        price: 499
     },
     {
         id: 6,
         name: "Pepperoni",
         desc: "Double pepperoni and extra pizza mozzarella.",
         img: "https://api.pizzahut.io/v1/content/en-ca/ca-1/images/pizza/pizza.pepperoni-lovers.69f7bdf7b6f50a87eb2886934fe0be9f.1.jpg?width=300",
-        price: 9.99
+        price: 399
+    },
+    {
+        id: 7,
+        name: "Steamed Cheeseburger",
+        desc: "BBQ sauce with seasoned grilled chicken",
+        img: "https://www.ketogenicforums.com/uploads/default/original/3X/4/1/414e8494f48a6b27e9758be09bf9e885fe5df70f.jpeg",
+        price: 79
+    },
+    {
+        id: 8,
+        name: " Classic Burger",
+        desc: "hamburger with ketchup, mustard and pickles" ,
+        img: "https://buy.am/media/image/ec/8b/f5/Classic-Classic-Burger.jpg",
+        price: 99
+    },
+    {
+        id: 9,
+        name: "Pepperoni Pizza Burger",
+        desc: "grilled burger covered with pepperoni, Mozzarella cheese and pizza sauce",
+        img: "https://www.dixiechikcooks.com/wp-content/uploads/2017/07/IMGP7686.jpg",
+        price: 89
+    },
+    {
+        id: 10,
+        name: "Greek Burger",
+        desc: "topped with herbed Feta cheese, black olives and onions",
+        img: "https://assets.kraftfoods.com/recipe_images/opendeploy/186793_640x428.jpg",
+        price: 129
+    },
+    {
+        id: 11,
+        name: "Barbecue Burger",
+        desc: "ground beef grilled with a tangy barbecue sauce and hot peppers",
+        img: "https://nadiashealthykitchen.com/wp-content/uploads/2019/05/vegan-cheesy-bbq-burger_6.jpg",
+        price: 499
+    },
+    {
+        id: 12,
+        name: "Sticky Burger ",
+        desc: "grilled burger spread with peanut butter, bacon and Jack cheese",
+        img: "https://images.squarespace-cdn.com/content/v1/5937745f1b10e34a80c14da1/1516013886913-OPCVUR8ZSPGYS6IUJ02E/ke17ZwdGBToddI8pDm48kOE-eDpsuqLgHS5WYR8-Kyd7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UeTxadt4UJJi61Ja24sDYFzI-4uTE5tYLkoe_Rt2fIJ5kvjJh3JJWsjdMZ66YIxKAg/Sticky+BBQ+Chicken+Burger+With+Steakhouse+Fries?width=300",
+        price: 299
     },
 ];
 
